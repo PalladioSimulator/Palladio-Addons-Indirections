3c3acad9d22fef762e8d3e47c2d643a8dd9de7c2
Dominik Werle <dominik.werle@kit.edu>
2019-09-25 10:23:34 +0200

    moved addParameterToStackFrameWithCopying to Helper

--- bundles/org.palladiosimulator.indirections.simulizar/src/org/palladiosimulator/indirections/simulizar/rdseffswitch/IndirectionsAwareRDSeffSwitch.java
+++ bundles/org.palladiosimulator.indirections.simulizar/src/org/palladiosimulator/indirections/simulizar/rdseffswitch/IndirectionsAwareRDSeffSwitch.java
@@ -1,16 +1,12 @@
 package org.palladiosimulator.indirections.simulizar.rdseffswitch;
 
-import java.util.List;
-import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.measure.Measure;
 import javax.measure.quantity.Duration;
 import javax.measure.unit.SI;
 
 import org.apache.log4j.Logger;
-import org.eclipse.emf.common.util.EList;
 import org.palladiosimulator.indirections.actions.AnalyseStackAction;
 import org.palladiosimulator.indirections.actions.ConsumeDataAction;
 import org.palladiosimulator.indirections.actions.CreateBirthDateAction;
@@ -25,25 +21,19 @@ import org.palladiosimulator.indirections.simulizar.measurements.IndirectionMeas
 import org.palladiosimulator.indirections.simulizar.measurements.TriggeredProxyProbe;
 import org.palladiosimulator.indirections.system.DataChannel;
 import org.palladiosimulator.indirections.util.IndirectionModelUtil;
+import org.palladiosimulator.indirections.util.IndirectionUtil;
 import org.palladiosimulator.indirections.util.IterableUtil;
 import org.palladiosimulator.indirections.util.MapUtil;
 import org.palladiosimulator.pcm.allocation.Allocation;
-import org.palladiosimulator.pcm.core.PCMRandomVariable;
 import org.palladiosimulator.pcm.core.composition.AssemblyContext;
-import org.palladiosimulator.pcm.parameter.VariableCharacterisation;
-import org.palladiosimulator.pcm.parameter.VariableUsage;
 import org.palladiosimulator.simulizar.exceptions.PCMModelInterpreterException;
 import org.palladiosimulator.simulizar.interpreter.ExplicitDispatchComposedSwitch;
 import org.palladiosimulator.simulizar.interpreter.InterpreterDefaultContext;
 import org.palladiosimulator.simulizar.runtimestate.SimulatedBasicComponentInstance;
 import org.palladiosimulator.simulizar.utils.SimulatedStackHelper;
 
-import de.uka.ipd.sdq.simucomframework.variables.EvaluationProxy;
-import de.uka.ipd.sdq.simucomframework.variables.StackContext;
 import de.uka.ipd.sdq.simucomframework.variables.exceptions.ValueNotInFrameException;
 import de.uka.ipd.sdq.simucomframework.variables.stackframe.SimulatedStackframe;
-import de.uka.ipd.sdq.stoex.AbstractNamedReference;
-import de.uka.ipd.sdq.stoex.analyser.visitors.StoExPrettyPrintVisitor;
 
 public class IndirectionsAwareRDSeffSwitch extends ActionsSwitch<Object> {
     private static final Logger LOGGER = Logger.getLogger(IndirectionsAwareRDSeffSwitch.class);
@@ -85,81 +75,6 @@ public class IndirectionsAwareRDSeffSwitch extends ActionsSwitch<Object> {
         this.parentSwitch = parentSwitch;
     }
 
-    /**
-     * Same as
-     * {@link SimulatedStackHelper#addParameterToStackFrame(SimulatedStackframe, EList, SimulatedStackframe)}
-     * but defaults for the parameters.
-     * 
-     * Additionally, it can copy all characterizations for a type by specifying a reference name of:
-     * input->output
-     * 
-     * @param parameterName
-     */
-    // TODO clean up and refactor
-    private static final void addParameterToStackFrameWithCopying(final SimulatedStackframe<Object> contextStackFrame,
-            final EList<VariableUsage> parameter, final String parameterName,
-            final SimulatedStackframe<Object> targetStackFrame) {
-
-        for (final VariableUsage variableUsage : parameter) {
-            if (variableUsage.getVariableCharacterisation_VariableUsage().isEmpty()) {
-                final AbstractNamedReference namedReference = variableUsage.getNamedReference__VariableUsage();
-                // TODO: move from convention quick hack to better solution
-                final String[] split = namedReference.getReferenceName().split("->");
-                if (split.length != 2) {
-                    throw new PCMModelInterpreterException(
-                            "If no variable chacterisations are present, name must be of form 'input->output'. Name is: "
-                                    + namedReference.getReferenceName());
-                }
-
-                final String inputPrefix = split[0] + ".";
-                final String outputPrefix = split[1] + ".";
-
-                final List<Entry<String, Object>> inputs = contextStackFrame.getContents().stream()
-                        .filter(it -> it.getKey().startsWith(inputPrefix)).collect(Collectors.toList());
-
-                if (inputs.size() == 0) {
-                    throw new PCMModelInterpreterException("Nothing found on stack frame for prefix '" + inputPrefix
-                            + "'. Available: " + contextStackFrame.getContents().stream().map(it -> it.getKey())
-                                    .collect(Collectors.joining(", ")));
-                }
-
-                inputs.forEach(it -> targetStackFrame
-                        .addValue(outputPrefix + it.getKey().substring(inputPrefix.length()), it.getValue()));
-                continue;
-            }
-
-            for (final VariableCharacterisation variableCharacterisation : variableUsage
-                    .getVariableCharacterisation_VariableUsage()) {
-
-                final PCMRandomVariable randomVariable = variableCharacterisation
-                        .getSpecification_VariableCharacterisation();
-
-                final AbstractNamedReference namedReference = variableCharacterisation
-                        .getVariableUsage_VariableCharacterisation().getNamedReference__VariableUsage();
-
-                final String id = new StoExPrettyPrintVisitor().doSwitch(namedReference).toString() + "."
-                        + variableCharacterisation.getType().getLiteral();
-
-                if (SimulatedStackHelper.isInnerReference(namedReference)) {
-                    targetStackFrame.addValue(id,
-                            new EvaluationProxy(randomVariable.getSpecification(), contextStackFrame.copyFrame()));
-                } else {
-                    targetStackFrame.addValue(id,
-                            StackContext.evaluateStatic(randomVariable.getSpecification(), contextStackFrame));
-                }
-
-                if (LOGGER.isDebugEnabled()) {
-                    try {
-                        LOGGER.debug("Added value " + targetStackFrame.getValue(id) + " for id " + id
-                                + " to stackframe " + targetStackFrame);
-                    } catch (final ValueNotInFrameException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        }
-    }
-
     @Override
     public Object caseEmitDataAction(final EmitDataAction action) {
         LOGGER.trace("Emit event action: " + action.getEntityName());
@@ -172,7 +87,7 @@ public class IndirectionsAwareRDSeffSwitch extends ActionsSwitch<Object> {
         final String parameterName = IterableUtil
                 .claimOne(action.getDataSourceRole().getEventGroup().getEventTypes__EventGroup())
                 .getParameter__EventType().getParameterName();
-        addParameterToStackFrameWithCopying(this.context.getStack().currentStackFrame(),
+        IndirectionUtil.addParameterToStackFrameWithCopying(this.context.getStack().currentStackFrame(),
                 action.getInputVariableUsages__CallAction(), parameterName, eventStackframe);
 
         // TODO: check cases in which getContents does not work
@@ -201,7 +116,7 @@ public class IndirectionsAwareRDSeffSwitch extends ActionsSwitch<Object> {
                             .claimOne(action.getDataSinkRole().getEventGroup().getEventTypes__EventGroup())
                             .getParameter__EventType().getParameterName();
                     LOGGER.trace("Parameter name: " + parameterName + " (thread: " + threadName + ")");
-                    addParameterToStackFrameWithCopying(contextStackframe,
+                    IndirectionUtil.addParameterToStackFrameWithCopying(contextStackframe,
                             action.getReturnVariableUsage__CallReturnAction(), parameterName,
                             this.context.getStack().currentStackFrame());
                     LOGGER.trace("Got stack frame: " + this.context.getStack().currentStackFrame().toString());
--- bundles/org.palladiosimulator.indirections.util/src/org/palladiosimulator/indirections/util/IndirectionUtil.java
+++ bundles/org.palladiosimulator.indirections.util/src/org/palladiosimulator/indirections/util/IndirectionUtil.java
@@ -1,17 +1,33 @@
 package org.palladiosimulator.indirections.util;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.pcm.core.PCMRandomVariable;
+import org.palladiosimulator.pcm.parameter.VariableCharacterisation;
+import org.palladiosimulator.pcm.parameter.VariableUsage;
 import org.palladiosimulator.pcm.repository.EventGroup;
 import org.palladiosimulator.pcm.repository.Parameter;
+import org.palladiosimulator.simulizar.exceptions.PCMModelInterpreterException;
 import org.palladiosimulator.simulizar.simulationevents.PeriodicallyTriggeredSimulationEntity;
+import org.palladiosimulator.simulizar.utils.SimulatedStackHelper;
 
 import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
+import de.uka.ipd.sdq.simucomframework.variables.EvaluationProxy;
+import de.uka.ipd.sdq.simucomframework.variables.StackContext;
+import de.uka.ipd.sdq.simucomframework.variables.exceptions.ValueNotInFrameException;
+import de.uka.ipd.sdq.simucomframework.variables.stackframe.SimulatedStackframe;
+import de.uka.ipd.sdq.stoex.AbstractNamedReference;
+import de.uka.ipd.sdq.stoex.analyser.visitors.StoExPrettyPrintVisitor;
 
 public final class IndirectionUtil {
+    private final static Logger LOGGER = Logger.getLogger(IndirectionUtil.class);
+    
     private IndirectionUtil() {
-
     }
 
     public static void validateStackframeStructure(final Map<String, Object> dataMap, final String parameterName) {
@@ -21,6 +37,80 @@ public final class IndirectionUtil {
             if (!entry.getKey().startsWith(parameterCharacterisationPrefix)) {
                 throw new IllegalArgumentException("Invalid characteristation for data frame: " + entry.getKey()
                         + ", expected characteristations for " + parameterName);
+            }
+        }
+    }
+
+    /**
+     * Same as
+     * {@link SimulatedStackHelper#addParameterToStackFrame(SimulatedStackframe, EList, SimulatedStackframe)}
+     * but defaults for the parameters.
+     * 
+     * Additionally, it can copy all characterizations for a type by specifying a reference name of:
+     * input->output
+     * 
+     * @param parameterName
+     */
+    public static final void addParameterToStackFrameWithCopying(final SimulatedStackframe<Object> contextStackFrame,
+            final EList<VariableUsage> parameter, final String parameterName,
+            final SimulatedStackframe<Object> targetStackFrame) {
+
+        for (final VariableUsage variableUsage : parameter) {
+            if (variableUsage.getVariableCharacterisation_VariableUsage().isEmpty()) {
+                final AbstractNamedReference namedReference = variableUsage.getNamedReference__VariableUsage();
+                // TODO: move from convention quick hack to better solution
+                final String[] split = namedReference.getReferenceName().split("->");
+                if (split.length != 2) {
+                    throw new PCMModelInterpreterException(
+                            "If no variable chacterisations are present, name must be of form 'input->output'. Name is: "
+                                    + namedReference.getReferenceName());
+                }
+
+                final String inputPrefix = split[0] + ".";
+                final String outputPrefix = split[1] + ".";
+
+                final List<Entry<String, Object>> inputs = contextStackFrame.getContents().stream()
+                        .filter(it -> it.getKey().startsWith(inputPrefix)).collect(Collectors.toList());
+
+                if (inputs.size() == 0) {
+                    throw new PCMModelInterpreterException("Nothing found on stack frame for prefix '" + inputPrefix
+                            + "'. Available: " + contextStackFrame.getContents().stream().map(it -> it.getKey())
+                                    .collect(Collectors.joining(", ")));
+                }
+
+                inputs.forEach(it -> targetStackFrame
+                        .addValue(outputPrefix + it.getKey().substring(inputPrefix.length()), it.getValue()));
+                continue;
+            }
+
+            for (final VariableCharacterisation variableCharacterisation : variableUsage
+                    .getVariableCharacterisation_VariableUsage()) {
+
+                final PCMRandomVariable randomVariable = variableCharacterisation
+                        .getSpecification_VariableCharacterisation();
+
+                final AbstractNamedReference namedReference = variableCharacterisation
+                        .getVariableUsage_VariableCharacterisation().getNamedReference__VariableUsage();
+
+                final String id = new StoExPrettyPrintVisitor().doSwitch(namedReference).toString() + "."
+                        + variableCharacterisation.getType().getLiteral();
+
+                if (SimulatedStackHelper.isInnerReference(namedReference)) {
+                    targetStackFrame.addValue(id,
+                            new EvaluationProxy(randomVariable.getSpecification(), contextStackFrame.copyFrame()));
+                } else {
+                    targetStackFrame.addValue(id,
+                            StackContext.evaluateStatic(randomVariable.getSpecification(), contextStackFrame));
+                }
+
+                if (LOGGER.isDebugEnabled()) {
+                    try {
+                        LOGGER.debug("Added value " + targetStackFrame.getValue(id) + " for id " + id
+                                + " to stackframe " + targetStackFrame);
+                    } catch (final ValueNotInFrameException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         }
     }
@@ -43,9 +133,10 @@ public final class IndirectionUtil {
         }
     }
 
-    public static PeriodicallyTriggeredSimulationEntity triggerPeriodically(SimuComModel model, double firstOccurrence, double delay,
+    public static PeriodicallyTriggeredSimulationEntity triggerPeriodically(SimuComModel model, double firstOccurrence,
+            double delay,
             Runnable taskToRun) {
-        
+
         return new PeriodicallyTriggeredSimulationEntity(model, firstOccurrence, delay) {
             @Override
             protected void triggerInternal() {
