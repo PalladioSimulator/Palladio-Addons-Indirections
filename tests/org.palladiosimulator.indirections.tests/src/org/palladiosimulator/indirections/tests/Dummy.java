3f593dee7d68a1ea22f5abe57ac40f3a52e1b599
Dominik Werle <dominik.werle@kit.edu>
2019-11-23 12:00:01 +0100

    added initial test project

---
+++ tests/org.palladiosimulator.indirections.tests/src/org/palladiosimulator/indirections/tests/Dummy.java
@@ -0,0 +1,19 @@
+package org.palladiosimulator.indirections.tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class Dummy {
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    void testDummy() {
+        fail("Not yet implemented");
+    }
+
+}
